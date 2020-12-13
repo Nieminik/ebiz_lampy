2cd74cbc4b3dc7d5bfbd1b80e66430a86e93d6a0
Dominik Niemiro <nieminik@protonmail.com>
2020-12-13 01:30:15 +0100

    Add fill stock

--- _scraper/upload.py
+++ _scraper/upload.py
@@ -105,6 +105,62 @@ def get_or_add_feature_value(
     return presta_id
 
 
+def fill_stocks(presta_api, source_id_to_presta_id, products_json):
+    """ This one only fills the existing stock_available entities based on the scraped data."""
+
+    def get_available_prod_id(ids):
+        for id in ids:
+            prod = get_object_from_presta(f"products/{id}", "product") 
+            if prod["associations"][f"{SA}s"].get(f"{SA}"):
+                # skip one with existing stock
+                continue
+            yield id
+
+    SA = "stock_available"
+    presta_ids = source_id_to_presta_id["products"].values()
+    prod_gen = get_available_prod_id(presta_ids)
+
+    for stock in get_objects_from_presta(f"{SA}s", f"{SA}"):
+        stock = get_object_from_presta(f'{SA}s/{stock["@id"]}', f"{SA}")
+        stock_prod_id = stock["id_product"]["#text"]
+
+        if stock_prod_id in presta_ids:
+            presta_prod_id = stock_prod_id
+        else:
+            try:
+                presta_prod_id = next(prod_gen)
+            except StopIteration:
+                # If the script enters here it means that stock.prod_id is pointing
+                # to a non-existent product and all the products have stock assigned.
+                # Continuing to ensure all the stocks with valid products are updated.
+                continue
+
+        source_id = {v: k for k, v in source_id_to_presta_id["products"].items()}[presta_prod_id]
+        json_product = next((x for x in products_json.values() if x["products_id"] == source_id), None)
+        if json_product is None:
+            continue
+        
+        first_word = json_product["quantity"].split(" ")[0]
+        try:
+            value = int(first_word)
+        except ValueError:
+            quantity = 21
+        else:
+            quantity = max(21, value)
+        
+        data = {
+            "id_product": presta_prod_id,
+            "id_product_attribute": stock["id_product_attribute"],
+            "id_shop": stock["id_shop"]["#text"],
+            "id_shop_group": stock["id_shop_group"],
+            "depends_on_stock": stock["depends_on_stock"],
+            "out_of_stock": stock["out_of_stock"],
+            "quantity": quantity,
+            "location": stock["location"]
+        }
+        edit_presta_object(f"{SA}s", stock["id"], data, f"{SA}")
+
+
 def main():
     object_types = [
         "manufacturers",
@@ -299,6 +355,8 @@ def main():
             presta_id = response["id"]
         name_to_presta_id["products"][prod_name] = presta_id
         source_id_to_presta_id["products"][prod_id] = presta_id
+
+    fill_stocks(presta_api, source_id_to_presta_id, products)
 
 
 if __name__ == "__main__":
