11233f63bfdda5907fd24df090ce6524a439fd68
Dominik Niemiro <dniemiro@protonmail.com>
2021-01-17 19:26:10 +0100

    Extend start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+sudo rm -rf docker/db/.db_data
+sudo rm -rf docker/ssl/*
 ./generate_cert.sh
 docker-compose up
